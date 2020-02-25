1932ea19265229fd6fb7175fdf9fb3a087c5267c
Binaya Chaudhary <binayachaudari@live.com>
2020-02-25 14:53:49 +0545

    addUser and checkUserExists modified

--- controller/users/checkUserExists.js
+++ controller/users/checkUserExists.js
@@ -1,11 +1,11 @@
 const User = require('../../models/Users');
 
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   const { email } = req.body
 
   //Check if email exists
-  User.findOne({ email }).then(user => {
+  await User.findOne({ email }).then(user => {
     if (user) {
       return next({
         status: 400,
