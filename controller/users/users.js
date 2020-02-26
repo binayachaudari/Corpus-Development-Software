6a26bad27e13714cb17fe781af6e84fbd981eb7f
Binaya Chaudhary <binayachaudari@live.com>
2020-02-26 12:14:50 +0545

    provide details of user added (addUser) & Filter users by their roles using req.query

--- controller/users/users.js
+++ controller/users/users.js
@@ -3,6 +3,20 @@ const user = require('../../models/Users');
 let allUsers = async (req, res, next) => {
   try {
     const listOfUsers = await user.find();
+
+    //Check if query string Exists
+    if (Object.keys(req.query).length !== 0) {
+
+      //Valid qusery role
+      if (req.query.role && ['admin', 'developer', 'linguist', 'reviewer'].includes(req.query.role.toLowerCase()))
+        return res.json(listOfUsers.filter(value => value.role.toLowerCase() === req.query.role.toLowerCase()));
+      else
+        return next({
+          status: 400,
+          message: "Invalid query: role, role includes [Admin, Developer, Linguist, Reviewer]"
+        });
+    }
+
     res.json(listOfUsers);
   } catch (error) {
     next({
