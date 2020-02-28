9d9446b3068885d685fe98a35fa00f29f9b09095
Binaya Chaudhary <binayachaudari@live.com>
2020-02-28 18:00:13 +0545

    checks if the assignment of task is valid

---
+++ middleware/isValidAssignment.js
@@ -0,0 +1,27 @@
+const User = require('../models/Users');
+
+module.exports = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ _id: req.body.assigned_to }).select('name email role');
+    req.body.assigned_by = req.user.id;
+
+    if (!user)
+      return next({
+        status: 404,
+        message: 'Such user does not exits.'
+      });
+
+    if (!['Linguist', 'Reviewer'].includes(user.role))
+      return next({
+        status: 400,
+        message: `Cannot assign task to ${user.role}`
+      })
+
+    next();
+  } catch (error) {
+    next({
+      status: 500,
+      message: error.message
+    })
+  }
+}
