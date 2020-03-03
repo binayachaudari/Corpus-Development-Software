8e5d701b241416320cce1a19669bcdca9f8639f5
Binaya Chaudhary <binayachaudari@live.com>
2020-03-03 20:25:18 +0545

    checkRole bug fix

--- middleware/checkRole.js
+++ middleware/checkRole.js
@@ -3,7 +3,7 @@ module.exports.restrictTo = (...roles) => {
     //Roles ['admin',...]
 
     if (!roles.includes(req.user.role))
-      return ({
+      return next({
         status: 403,
         message: 'You do not have permission to perform this action.'
       });
