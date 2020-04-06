16c8092dfb55cff4aa718281336ef2abdf6808f3
Binaya Chaudhary <binayachaudari@live.com>
2020-04-06 16:07:15 +0545

    Linguist dashboard complete

---
+++ client/src/components/dashboard/linguist/FileStatusComponent.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { Table, Spinner } from 'react-bootstrap'
+import { assignedTranslateFiles, completedTranslationReview } from '../admin/FileStatusComponent'
+
+const FileStatusComponent = ({ headProps, tableCaption, loadData, dataFiles, loading, showTab }) => {
+  useEffect(() => {
+    loadData();
+  }, [loadData]);
+
+  return (
+    <>
+      {loading ?
+        <div className="d-flex justify-content-center">
+          <Spinner animation="grow" role="status">
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        </div> :
+        <Table className="text-center bg-light" responsive bordered>
+          <caption className="font-weight-lighter font-italic">{tableCaption}</caption>
+          <thead className="table-dark" >
+            <tr>
+              {headProps.map((item, index) => (
+                <th key={index} className="align-middle">{item}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {showTab === 'translation_assigned_files' && assignedTranslateFiles(dataFiles)}
+            {showTab === 'under_translation' && assignedTranslateFiles(dataFiles)}
+            {showTab === 'translated' && completedTranslationReview(dataFiles, 'Translated')}
+          </tbody>
+        </Table>
+      }
+    </>
+  )
+}
+
+FileStatusComponent.propTypes = {
+  headProps: PropTypes.array.isRequired,
+  tableCaption: PropTypes.string.isRequired,
+  loadData: PropTypes.func.isRequired,
+  dataFiles: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+  showTab: PropTypes.string.isRequired
+}
+
+export default FileStatusComponent
---
+++ client/src/components/dashboard/linguist/LinguistDashboardContents.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Tab, Col, Row, Nav } from 'react-bootstrap'
+import { connect } from 'react-redux';
+import FileStatusComponent from './FileStatusComponent'
+import { translationGetMyFiles } from '../../../actions/user_files'
+
+const assignedTranslationHead = ['File ID', 'No. of Sentences', 'Assigned To',
+  'Assigned By', 'Filename', 'Assigned Date', 'Deadline', 'Remaining Time'];
+
+const translationCompleteHead = [...assignedTranslationHead];
+translationCompleteHead[4] = 'Translated Filenames';
+translationCompleteHead[5] = 'Submitted';
+translationCompleteHead[7] = 'Remarks';
+
+const LinguistDashboardContents = ({ user_files: { loading, my_files }, translationGetMyFiles }) => {
+  const currentPill = localStorage.__currentTraPill__DASH || 'translation_assigned_files';
+  const onSelect = (pill) => {
+    localStorage.setItem('__currentTraPill__DASH', pill);
+  }
+  return (
+    <Tab.Container id="left-tabs-example" unmountOnExit={true} onSelect={onSelect} defaultActiveKey={currentPill}>
+      <Row className="mt-5">
+        <Col sm={2}>
+          <Nav variant="pills" className="flex-column position-fixed">
+            <Nav.Item>
+              <Nav.Link eventKey="translation_assigned_files" >Translation Assigned</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="under_translation">Under Translation</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="translated">Translated</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+        <Col className="text-justify" sm={10}>
+          <Tab.Content>
+            <Tab.Pane eventKey="translation_assigned_files">
+              <FileStatusComponent loadData={translationGetMyFiles}
+                headProps={assignedTranslationHead}
+                tableCaption="List of assigned files (Translation)"
+                dataFiles={my_files.filter(file => file.status === 'assigned')}
+                loading={loading}
+                showTab="translation_assigned_files"
+              />
+            </Tab.Pane>
+            <Tab.Pane eventKey="under_translation">
+              <FileStatusComponent loadData={translationGetMyFiles}
+                headProps={assignedTranslationHead}
+                tableCaption="List of files Under-Translation"
+                dataFiles={my_files.filter(file => file.status === 'under_translation')}
+                loading={loading}
+                showTab="under_translation"
+              />
+            </Tab.Pane>
+            <Tab.Pane eventKey="translated">
+              <FileStatusComponent loadData={translationGetMyFiles}
+                headProps={translationCompleteHead}
+                tableCaption="List of Translated Files"
+                dataFiles={my_files.filter(file => file.status === 'translation_complete')}
+                loading={loading}
+                showTab="translated"
+              />
+            </Tab.Pane>
+          </Tab.Content>
+        </Col>
+      </Row>
+    </Tab.Container>
+  )
+}
+
+LinguistDashboardContents.propTypes = {
+  user_files: PropTypes.object.isRequired,
+  translationGetMyFiles: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+  user_files: state.user_files
+})
+
+export default connect(mapStateToProps, { translationGetMyFiles })(LinguistDashboardContents)
