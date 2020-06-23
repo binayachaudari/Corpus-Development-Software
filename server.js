38b11734e39e4cbb61b78274d7ca6f7845da8c66
Binaya Chaudhary <binayachaudari@live.com>
2020-06-23 18:07:28 +0545

    Serve static files bug fix

--- server.js
+++ server.js
@@ -36,13 +36,14 @@ app.use('/api', require('./routes/api'));
 /**
  * Static Files
  */
+app.use('/reports', express.static(path.resolve(__dirname, 'Reports')));
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-app.use('/reports', express.static(path.resolve(__dirname, 'Reports')));
 
 /**
  * Application Middleware
